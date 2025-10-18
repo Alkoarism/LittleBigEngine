f25e468a03a69a6b5d91f9b56cc758164fb358f4
alkoarism <69710741+Alkoarism@users.noreply.github.com>
2025-10-18 16:31:10 -0300

    Preparing TestGround for Resource Manager development

--- v0_04-Core_Consolidation/src/main.cpp
+++ v0_04-Core_Consolidation/src/main.cpp
@@ -60,10 +60,6 @@ int main() {
 	// glfw: setup
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-	glfwSetCursorPosCallback(window, mouse_callback);
-	glfwSetScrollCallback(window, scroll_callback);
-
-	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	// glad: load all OpenGL function pointers
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
@@ -100,97 +96,8 @@ int main() {
 		0, 2, 3
 	};
 	fontIBO = std::make_unique<IndexBuffer>(fontIndices, 6);
-///*
-	// vertices definition -------------------------------------------------------
-	std::vector<float> cube_vData = {
-		//vertex			  //Vertex Normal		//texture	r	
-		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,	0.0f, 0.0f,	// 0
-		 0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,	1.0f, 0.0f,	// 1
-		 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,	1.0f, 1.0f,	// 2
-		-0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,	0.0f, 1.0f,	// 3
-
-		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,	0.0f, 0.0f,	// 4
-		 0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,	1.0f, 0.0f,	// 5
-		 0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,	1.0f, 1.0f,	// 6
-		-0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,	0.0f, 1.0f,	// 7
-
-		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,	1.0f, 0.0f,	// 8
-		-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,	1.0f, 1.0f,	// 9
-		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,	0.0f, 1.0f,	// 10
-		-0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,	0.0f, 0.0f,	// 11
-
-		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,	1.0f, 0.0f,	// 12
-		 0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,	1.0f, 1.0f,	// 13
-		 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,	0.0f, 1.0f,	// 14
-		 0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,	0.0f, 0.0f,	// 15
-
-		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,	0.0f, 1.0f,	// 16
-		 0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,	1.0f, 1.0f,	// 17
-		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,	1.0f, 0.0f,	// 18
-		-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,	0.0f, 0.0f,	// 19
-
-		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,	0.0f, 1.0f,	// 20
-		 0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,	1.0f, 1.0f,	// 21
-		 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,	1.0f, 0.0f,	// 22
-		-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,	0.0f, 0.0f	// 23
-	};
-	
-	std::vector<float> light_vData = {
-		//vertex			  //texture		
-		-0.5f, -0.5f, -0.5f,	// 0
-		 0.5f, -0.5f, -0.5f,	// 1
-		 0.5f,  0.5f, -0.5f,	// 2
-		-0.5f,  0.5f, -0.5f,	// 3
-
-		-0.5f, -0.5f,  0.5f,	// 4
-		 0.5f, -0.5f,  0.5f,	// 5
-		 0.5f,  0.5f,  0.5f,	// 6
-		-0.5f,  0.5f,  0.5f,	// 7
-
-		-0.5f,  0.5f,  0.5f,	// 8
-		-0.5f,  0.5f, -0.5f,	// 9
-		-0.5f, -0.5f, -0.5f,	// 10
-		-0.5f, -0.5f,  0.5f,	// 11
-
-		 0.5f,  0.5f,  0.5f,	// 12
-		 0.5f,  0.5f, -0.5f,	// 13
-		 0.5f, -0.5f, -0.5f,	// 14
-		 0.5f, -0.5f,  0.5f,	// 15
-
-		-0.5f, -0.5f, -0.5f,	// 16
-		 0.5f, -0.5f, -0.5f,	// 17
-		 0.5f, -0.5f,  0.5f,	// 18
-		-0.5f, -0.5f,  0.5f,	// 19
-
-		-0.5f,  0.5f, -0.5f,	// 20
-		 0.5f,  0.5f, -0.5f,	// 21
-		 0.5f,  0.5f,  0.5f,	// 22
-		-0.5f,  0.5f,  0.5f		// 23
-	};
-
-	std::vector<unsigned int> vertexIndices = {
-		//cube faces
-		0, 1, 2,		0, 3, 2,
-		4, 5, 6,		4, 7, 6,
-		8, 9, 10,		8, 11, 10,
-		12, 13, 14,		12, 15, 14,
-		16, 17, 18,		16, 19, 18,
-		20, 21, 22,		20, 23, 22
-	};
 
 	// Model (vertex and buffers) configurations ---------------------------------
-	Model& lightCube = Things::LoadModel("lightCube");
-	Model& containerCube = Things::LoadModel("containerCube");
-
-	Mesh lightMesh(light_vData, std::vector<unsigned int>{3});
-	lightCube.PushMesh(lightMesh.GetVertexData(), lightMesh.GetVertexLayout());
-	lightCube.SetIndexBuffer(vertexIndices);
-	
-	Mesh cubeMesh(cube_vData, std::vector<unsigned int>{3,3,2});
-	containerCube.PushMesh(cubeMesh.GetVertexData(), cubeMesh.GetVertexLayout());
-	containerCube.SetIndexBuffer(vertexIndices);
-	//
-//*/
 
 	// texture handling ----------------------------------------------------------
 	const Bitmap& atlasBMP = fontAtlas->GetBitmap();
@@ -204,14 +111,6 @@ int main() {
 	atlasTexture.SetPar(GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 	atlasTexture.SetPar(GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	atlasTexture.Load(atlasBMP.GetRawData(), atlasBMP.GetWidth(), atlasBMP.GetRows());
-	
-///*
-	Texture& container = Things::LoadTexture("container", "res\\textures\\container.jpg", true);
-	container.SetPar(GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	container.SetPar(GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	container.SetPar(GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	container.SetPar(GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-//*/
 
 	// initialization before rendering -------------------------------------------
 	Shader& fontShader = Things::LoadShader(
@@ -221,29 +120,7 @@ int main() {
 	
 	fontShader.SetUniform("projection", fontProjection);
 	fontShader.SetUniform("model", fontModel);
-///*
-	Shader& light_shader = Things::LoadShader(
-		"light_shader", 
-		"res\\shaders\\lightSource.vert", 
-		"res\\shaders\\lightSource.frag");
-	
-	Shader& test_shader = Things::LoadShader(
-		"test_shader", 
-		"res\\shaders\\test.vert", 
-		"res\\shaders\\test.frag");
 
-	test_shader.SetUniform("objectColor", glm::vec3(1.0f, 0.5f, 0.31f));
-	test_shader.SetUniform("lightColor", glm::vec3(1.0f, 1.0f, 1.0f));
-
-	test_shader.SetUniform("material.ambient", glm::vec3(1.0f, 0.5f, 0.31f));
-	test_shader.SetUniform("material.diffuse", glm::vec3(1.0f, 0.5f, 0.31f));
-	test_shader.SetUniform("material.specular", glm::vec3(0.5f, 0.5f, 0.5f));
-	test_shader.SetUniform("material.shininess", 32.0f);
-
-	test_shader.SetUniform("light.ambient", glm::vec3(0.1f, 0.1f, 0.1f));
-	test_shader.SetUniform("light.diffuse", glm::vec3(0.5f, 0.5f, 0.5f));
-	test_shader.SetUniform("light.specular", glm::vec3(1.0f, 1.0f, 1.0f));
-//*/
 	// render loop (happens every frame) -----------------------------------------
 	while (!glfwWindowShouldClose(window)) {
 		// -> frame time tracker
@@ -255,43 +132,23 @@ int main() {
 		Renderer::SetRender3D(true);
 		Renderer::RenderConfig(0.4f, 0.4f, 0.4f);
 		glEnable(GL_DEPTH_TEST);
-///*
+
 		// ---> world config
-		glm::mat4 projection3D = glm::perspective
-		(glm::radians(camera.Zoom), (float)screenWidth / (float)screenHeight, 0.1f, 100.0f);
-		Renderer::SetProjection(projection3D);
 
 		// ---> camera config
-		glm::mat4 view3D = camera.GetViewMatrix();
-		Renderer::SetView(view3D);
-		test_shader.SetUniform("viewPos", camera.Position);
 
 		// ---> model positioning
-		glm::mat4 model3D = glm::mat4(1.0f);
-		Renderer::SetModel(model3D);
-		glActiveTexture(GL_TEXTURE0);
-		container.Bind();
-		//test_shader.SetUniform("textColor", glm::vec3(sin(glfwGetTime()), 1.0f, 1.0f));
-		Renderer::Render(containerCube.GetVertexArray(), containerCube.GetIBO(), test_shader);
-
-		glm::vec3 lightPos(1.5 * sin(glfwGetTime()), 1.0f, 1.5 * cos(glfwGetTime()));
-		test_shader.SetUniform("light.position", lightPos);
-		model3D = glm::translate(model3D, lightPos);
-		model3D = glm::scale(model3D, glm::vec3(0.2f));
-		Renderer::SetModel(model3D);
-		Renderer::Render(lightCube.GetVertexArray(), lightCube.GetIBO(), light_shader);
-//*/
 		glDisable(GL_DEPTH_TEST);
 
 		// ---> font rendering
-		Renderer::SetRender3D(false);
-		bool shouldBlend = textBlending;
-		if (shouldBlend) glEnable(GL_BLEND);
+		Renderer::SetRender3D(false);			
+		bool shouldBlend = textBlending;		
+		if (shouldBlend) glEnable(GL_BLEND);	//Blending control
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 		glActiveTexture(GL_TEXTURE0);
 		atlasTexture.Bind();
 		RenderText(fontShader, "lord.sidious@deathstar", 10.0f, 20.0f, 1.0f, glm::vec3(1.0f, 1.0f, 0.0f));
-		if (shouldBlend) glDisable(GL_BLEND);
+		if (shouldBlend) glDisable(GL_BLEND);	//Blending control
 
 		// -> check and call events and swap the buffers
 		glfwSwapBuffers(window);
@@ -308,36 +165,6 @@ void processInput(GLFWwindow* window) {
 
 	if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS) textBlending = false;
 	else textBlending = true;
-
-	const float cameraSpeed = 2.5f * Renderer::GetDeltaTime();
-	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-		camera.ProcessKeyboard(FORWARD, Renderer::GetDeltaTime());
-	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-		camera.ProcessKeyboard(BACKWARD, Renderer::GetDeltaTime());
-	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-		camera.ProcessKeyboard(LEFT, Renderer::GetDeltaTime());
-	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-		camera.ProcessKeyboard(RIGHT, Renderer::GetDeltaTime());
-}
-
-void mouse_callback(GLFWwindow* window, double xPos, double yPos) {
-	if (firstMouse) {
-		lastX = xPos;
-		lastY = yPos;
-		firstMouse = false;
-	}
-
-	float xOffset = xPos - lastX;
-	float yOffset = lastY - yPos;
-
-	lastX = xPos;
-	lastY = yPos;
-
-	camera.ProcessMouseMovement(xOffset, yOffset);
-}
-
-void scroll_callback(GLFWwindow* window, double xOffset, double yOffset) {
-	camera.ProcessMouseScroll(yOffset);
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
